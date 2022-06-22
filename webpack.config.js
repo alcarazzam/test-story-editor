fc3d1a7544e0d87da74326d8e5d58acaf20435af
Manuel Alcaraz Zambrano <manuelalcarazzam@gmail.com>
2022-06-22 13:37:36 +0200

    add source map support

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true
     },
+    devtool: "eval-source-map",
     optimization: {
         moduleIds: "deterministic",
         runtimeChunk: "single",
